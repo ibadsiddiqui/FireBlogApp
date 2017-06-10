d403d14204f5017f36f17866feaa51dfe342c224
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-06-10 20:06:56 +0500

    SimpleBlog file created

---
+++ app/src/main/java/SimpleBlog.java
@@ -0,0 +1,8 @@
+import android.app.Application;
+
+/**
+ * Created by IBAD SIDDIQUI on 6/10/2017.
+ */
+
+public class SimpleBlog extends Application {
+}
--- app/src/main/java/com/ibadsiddiqui01outlook/fireblog/LoginActivity.java
+++ app/src/main/java/com/ibadsiddiqui01outlook/fireblog/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.ibadsiddiqui01outlook.fireblog;
 
-import android.os.Build;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -26,25 +26,37 @@ public class LoginActivity extends AppCompatActivity {
     private EditText mLoginEmailField;
     private EditText mLoginPasswordField;
     private Button mLoginBtn;
+    private Button mSignUpBtn;
+    private ProgressDialog mProgressDialog;
 
     private FirebaseAuth mAuth;
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabaseUsers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mLoginEmailField = (EditText)findViewById(R.id.loginEmailField);
         mLoginPasswordField = (EditText)findViewById(R.id.loginPasswordField);
+        mLoginBtn = (Button)findViewById(R.id.loginBtn);
+        mProgressDialog = new ProgressDialog(this);
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkLogin();
             }
         });
-
+        mSignUpBtn = (Button)findViewById(R.id.signUpBtn);
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                signUpIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(signUpIntent);
+            }
+        });
     }
 
     private void checkLogin() {
@@ -52,13 +64,17 @@ public class LoginActivity extends AppCompatActivity {
         String password = mLoginPasswordField.getText().toString();
 
         if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            mProgressDialog.setMessage("Checking Login");
+            mProgressDialog.show();
+
             mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
                         checkUserExist();
+                        mProgressDialog.dismiss();
                     }else{
+                        mProgressDialog.dismiss();
                         Toast.makeText(LoginActivity.this, "Error in Login", Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -68,11 +84,13 @@ public class LoginActivity extends AppCompatActivity {
 
     private void checkUserExist() {
         final String UserID = mAuth.getCurrentUser().getUid();
-        mDatabase.addValueEventListener(new ValueEventListener() {
+        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if(dataSnapshot.hasChild(UserID)){
-
+                    Intent mIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(mIntent);
                 }
             }
 
@@ -80,6 +98,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        })
+        });
     }
 }
