50fe6d06d9522582dae438417be9a7ac4357b6f3
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-06-22 00:13:16 +0500

    Gallery click option given to setup activity

--- app/src/main/java/com/ibadsiddiqui01outlook/fireblog/SetUpActivity.java
+++ app/src/main/java/com/ibadsiddiqui01outlook/fireblog/SetUpActivity.java
@@ -1,13 +1,45 @@
 package com.ibadsiddiqui01outlook.fireblog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SetUpActivity extends AppCompatActivity {
 
+
+    private ImageButton mSetupImageBtn;
+    private EditText mNameField;
+    private Button mSubmitBtn;
+    private static final int GALLERY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up);
+
+        mSetupImageBtn = (ImageButton)findViewById(R.id.setupProfileImageButton);
+        mNameField = (EditText)findViewById(R.id.nameField);
+        mSubmitBtn = (Button)findViewById(R.id.setupSubmitButton);
+
+        mSetupImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery_Intent = new Intent();
+                gallery_Intent.setAction(Intent.ACTION_GET_CONTENT);
+                gallery_Intent.setType("images/*");
+                startActivityForResult(gallery_Intent, GALLERY_REQUEST);
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
